9d3d12a75eca1184f396634726f5f46a72a01655
l-chapus <louka.chapus@gmail.com>
2023-02-14 18:16:12 +0100

    Ajout d'un dossier Java pour les futurs programme, ainsi que deux classes Java simple. Enfin ajout d'un Makefile comme en C++ et ajout des fichiers .class dans la liste d'exeption (ils sont issues de la compilation en Java)

---
+++ 3A/Java/test.java
@@ -0,0 +1,13 @@
+public class test{
+
+	public static void main(String[] args) {
+		System.out.println("Bonjour");
+	}
+}
+
+class test2{
+
+	public static void main(String[] args) {
+		System.out.println("Bonsoir");
+	}
+}
