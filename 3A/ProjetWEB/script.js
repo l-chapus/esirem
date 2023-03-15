058ae36523bb611cac17fae618ce1205afd87ded
l-chapus <louka.chapus@gmail.com>
2023-03-15 20:47:06 +0100

    Ajout du projet de WEB (pour l'instant c'est la page que j'avais faite en terminal)

---
+++ 3A/ProjetWEB/script.js
@@ -0,0 +1,9 @@
+let clique  = document.getElementById('g1');
+let clique2  = document.getElementById('g2');
+
+clique.addEventListener('click', alerte);
+clique2.addEventListener('click', alerte);
+
+function alerte(){
+	alert('test');
+}
